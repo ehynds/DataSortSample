ccc20380bff23ee2f36b4934a41bae5b4ce5e60b
Matt Propst <propstm@gmail.com>
2014-04-04 21:06:09 -0400

    Initial Commit

---
+++ DataSortSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DataSortSample
+//
+//  Created by Matthew Propst on 4/4/14.
+//  Copyright (c) 2014 Matthew Propst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
